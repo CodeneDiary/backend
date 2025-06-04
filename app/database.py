ef2f32b96c39faf1b33f61855cd580de0c560bf3
MinjiKnG <kangminji5017@gmail.com>
2025-06-04 11:52:01 +0900

    db_url

--- app/database.py
+++ app/database.py
@@ -5,7 +5,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # SQLite DB 연결 설정
-DATABASE_URL = "sqlite:///./diary.db"
+DATABASE_URL = "sqlite:///./app/diary.db"
 
 engine = create_engine(
     DATABASE_URL, connect_args={"check_same_thread": False}
